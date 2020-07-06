278f3d2434cdfb7548fef2d7f257bb181e63590f
Dennis Valdes <dennis.valdez@anima.edu.uy>
2020-07-06 19:03:33 -0300

    Ordenando sintaxis

--- server/build/database.js
+++ server/build/database.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
-const pool = new pg_1.Pool({ host: 'localhost',
+const pool = new pg_1.Pool({
+    host: 'localhost',
     user: 'postgres',
     password: 'root',
     database: 'sline',
-    port: 5432 });
+    port: 5432
+});
 exports.default = pool;
--- server/src/database.ts
+++ server/src/database.ts
@@ -1,9 +1,11 @@
 import {Pool} from 'pg';
 
-const pool = new Pool({ host: 'localhost',
-user: 'postgres',
-password: 'root',
-database: 'sline',
-port: 5432});
+const pool = new Pool({ 
+    host: 'localhost',
+    user: 'postgres',
+    password: 'root',
+    database: 'sline',
+    port: 5432
+});
 
-export default pool
+export default pool;
