e2a6cb2f3738d3ea334270a8f729486c208f3902
DennisValdes <dennis.valdez@anima.edu.uy>
2020-07-08 14:20:57 -0300

    Fixe en la ruta de userRoutes
    Estaba mal la ruta del controller

--- server/build/routes/userRoutes.js
+++ server/build/routes/userRoutes.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const userController_1 = __importDefault(require("./../controllers/userController"));
+const usercontroller_1 = __importDefault(require("./../controllers/usercontroller"));
 class UserRoutes {
     constructor() {
         this.router = express_1.Router();
@@ -12,15 +12,15 @@ class UserRoutes {
     }
     config() {
         //list users
-        this.router.get('/', userController_1.default.getUsers);
+        this.router.get('/', usercontroller_1.default.getUsers);
         //search by id
-        this.router.get('/:dato', userController_1.default.getUserById);
+        this.router.get('/:dato', usercontroller_1.default.getUserById);
         //SignUp
-        this.router.post('/', userController_1.default.createUser);
+        this.router.post('/', usercontroller_1.default.createUser);
         //borrar
-        this.router.delete('/:dato', userController_1.default.BorrarUsuario);
+        this.router.delete('/:dato', usercontroller_1.default.BorrarUsuario);
         //actualizar datos por nombre
-        this.router.put('/:dato', userController_1.default.ActualizarUsuario);
+        this.router.put('/:dato', usercontroller_1.default.ActualizarUsuario);
     }
 }
 const userRoutes = new UserRoutes();
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import userController from './../controllers/userController';
+import userController from './../controllers/usercontroller';
 
 class UserRoutes {
 
