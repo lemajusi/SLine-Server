fc28850ff740acde52d8e836afc14b1e206343f1
DennisValdes <dennis.valdez@anima.edu.uy>
2020-07-09 09:18:44 -0300

    Update casoRoutes.js

--- server/build/routes/casoRoutes.js
+++ server/build/routes/casoRoutes.js
@@ -1,13 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const casocontroller_1 = __importDefault(require("../controllers/casocontroller"));
 class CasoRoutes {
     constructor() {
         this.router = express_1.Router();
         this.config();
     }
     config() {
-        this.router.get('/');
+        this.router.get('/', casocontroller_1.default.getCasos);
+        this.router.get('/', casocontroller_1.default.getCasoById);
     }
 }
 const casoRoutes = new CasoRoutes();
