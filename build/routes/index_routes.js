d1854c040a03d62780936c8d5a21b09475ae36ae
lemajusi <lmartinez16access@gmail.com>
2020-08-19 13:31:53 -0300

    Exports

--- build/routes/index_routes.js
+++ build/routes/index_routes.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.indexRoutes = void 0;
 const express_1 = require("express");
 const indexcontroller_1 = __importDefault(require("../controllers/indexcontroller"));
 class IndexRoutes {
--- build/routes/user_routes.js
+++ build/routes/user_routes.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.userRoutes = void 0;
 const express_1 = require("express");
 const user_controller_1 = require("../controllers/user_controller");
 class UserRoutes {
