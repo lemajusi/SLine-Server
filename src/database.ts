c1814879420cf1516f174e3d62f6c041922724ac
DennisValdes <dennis@decemberlabs.com>
2020-08-27 13:45:50 -0300

    Fixes
    Agregue el link de la base de datos online, por ahora anda

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,11 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'root',
-    database: 'sline',
-    port: 5432
+    connectionString: 'postgresql://upphnvzvzbuhtfajbko2:sVDq4yd2cXdLXfgC8Xwv@blnh5b91ffx6fdsceb49-postgresql.services.clever-cloud.com:5432/blnh5b91ffx6fdsceb49',
+    // user: 'postgres',
+    // host: 'localhost',
+    // password: 'root',
+    // database: 'sline',
+    // port: 5432,
+    connectionTimeoutMillis: 2000,
 });
