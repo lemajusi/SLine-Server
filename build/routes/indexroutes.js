0c657fc140b29785bc2df234e82a529f308d8ba1
lemajusi <49279683+lemajusi@users.noreply.github.com>
2020-09-19 20:19:59 -0300

    commit

--- build/routes/indexroutes.js
+++ build/routes/indexroutes.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.indexRoutes = void 0;
 const express_1 = require("express");
-const indexcontroller_1 = require("../controllers/indexcontroller");
+const indexController_1 = require("../controllers/indexController");
 exports.indexRoutes = new class IndexRoutes {
     constructor() {
         this.router = express_1.Router();
         this.config();
     }
     config() {
-        this.router.get('/', indexcontroller_1.indexController.index);
+        this.router.get('/', indexController_1.indexController.index);
     }
 };
